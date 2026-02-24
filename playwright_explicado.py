ce3df7a9b215f985869b5ce84713bcb3dfaf335a
Egurt-0 <eduardogurovsky@gmail.com>
2026-02-24 16:54:02 -0300

    playwright_explained_teste

--- playwright_explicado.py
+++ playwright_explicado.py
@@ -6,13 +6,14 @@ async def main():
         # como arrumar o laboratorio antes de comecar o esperimento
         browser = await p.chromium.launch()# ele vem com headless=True, isso faz nao abrir o navegador, mas caso queiram so colocar no launch(headless=False)
         page = await browser.new_page() # criando a page, que e aonde voce navega,Clica,Le conteudo, executa JavaScript
-        await page.goto("https://bileto.sympla.com.br/event/115660/d/362549") # fazendo a requisicao executando o JavaSript 
+        await page.goto("https://bileto.sympla.com.br/event/115660/d/362549") # fazendo a requisicao pela url.
+        # quando voce nao define wait_until e o timeout, o proprio playwright decide por voce, e o default é await page.goto(url, wait_until="load", timeout=30000) e voce pode mudar caso necessario
         precos = await page.locator('span.range-ticket-price').all_inner_texts() # usar o (.) indica a classe se for id usa (#) tipo: span#range-ticket-price
         #precos = await page.locator('span:has-text("R$")').all_inner_texts()  = Busca todos os spans que contenham "R$" no texto
         print(precos)
         await browser.close()
 
-asyncio.run(main())
+asyncio.run(main()) # para executar precisa usar o asyncio, para fazer o script ser executado de maneira assincrona
 
 
 # tudo que conversa com o navegador é ASSINCRONO
