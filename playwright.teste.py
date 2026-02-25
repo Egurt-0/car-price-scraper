591d2eefaa1a8b68fabeaa4e8dcc1d49e2c03081
Egurt-0 <eduardogurovsky@gmail.com>
2026-02-25 11:54:15 -0300

    Getting_prices_rascunho_projeto

--- playwright.teste.py
+++ playwright.teste.py
@@ -6,7 +6,8 @@ async def site_personagens():
         browser = await p.chromium.launch()
         page = await browser.new_page()
         await page.goto("https://www.techtudo.com.br/listas/2026/02/10-animes-em-que-o-protagonista-morre-mas-valem-a-pena-assistir-streaming.ghtml?utm_source=SaibaMaisMidArticle_Tech_Tudo&utm_content=TechTudo%252Fmagazine%252FTechTudo&interno_origem=mid-article-saiba-mais-techtudo",
-                        wait_until="domcontentloaded", timeout=60000)
+                        wait_until="domcontentloaded", timeout=120000)
+        page.set_default_timeout(timeout=120000)
         titulo = await page.title()
         print(titulo)
         await browser.close()
--- rascunho_projeto.py
+++ rascunho_projeto.py
@@ -1,2 +1,23 @@
+import asyncio
+from playwright.async_api import async_playwright
 
+dict = {
+    "Nome": str,
+    "Preco": int
+}
 
+async def coletando_precos():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page()
+        await page.goto("https://www.webmotors.com.br/carros/estoque/jeep/commander?autocomplete=jeep%20comander%20T270&autocompleteTerm=Jeep%20Commander&tipoveiculo=carros&marca1=JEEP&modelo1=COMMANDER&page=1",
+            wait_until="load", timeout=60000)
+        page.set_default_timeout(timeout=60000)
+        card_locator = page.locator('._BodyItemSpaceBetween_1wsqi_75') # ele ainda nao procurou nada, por isso nao tem await nem inner_text
+        todos_carros = await card_locator.all() # apartir daqui ele varre o hmtl e transforma em uma lista
+        print(f"foram encotrados {len(todos_carros)} cards de carros")
+        for carro in todos_carros:
+            precos = await carro.locator('p._web-subtitle-medium_qtpsh_69:has-text("R$")').inner_text()
+            print(f"Preco Atual: {precos}")
+        await browser.close()
+asyncio.run(coletando_precos())
