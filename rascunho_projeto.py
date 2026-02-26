7225b7ffa00ca694ab89810601b1d654d3c1c08f
Egurt-0 <eduardogurovsky@gmail.com>
2026-02-26 19:10:14 -0300

    Adicionando_sites_rascunho

--- rascunho_projeto.py
+++ rascunho_projeto.py
@@ -6,21 +6,39 @@ dict = {
     "Preco": int
 }
 
-async def coletando_precos_wemotors():
+async def coletando_precos_napista():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False)
         page = await browser.new_page()
-        await page.goto("https://www.webmotors.com.br/ofertas/feiroes/ofertassantander/carros/estoque?feirao=Show%20de%20ofertas%20Santander&tipoveiculo=carros&page=1",
+        await page.goto("https://napista.com.br/busca/carro/ate-30000-reais?utm_source=GoogleAds&utm_medium=PMax&utm_campaign=PMax&gclsrc=aw.ds&utm_source=google&utm_medium=cpc&gad_source=1&gad_campaignid=22474505263&gclid=CjwKCAiA-__MBhAKEiwASBmsBGwllQ9e-AUFWUYrmqTDlTrCdKabk8AuHS4pLKGLhdDh3EMEJGfUyxoCAVEQAvD_BwE&pn=1",
             wait_until="load", timeout=60000)
         page.set_default_timeout(timeout=60000)
-        card_locator = page.locator('._BodyItemSpaceBetween_1wsqi_75') # ele ainda nao procurou nada, por isso nao tem await nem inner_text
+        card_locator = page.locator('.styles_listingCardContentWrapper__eTihz') # ele ainda nao procurou nada, por isso nao tem await nem inner_text
         todos_carros = await card_locator.all() # apartir daqui ele varre o hmtl e transforma em uma lista
         print(f"foram encotrados {len(todos_carros)} cards de carros")
         for carro in todos_carros:
-            precos = await carro.locator('p._web-subtitle-medium_qtpsh_69:has-text("R$")').inner_text()
-            print(f"Preco Atual: {precos}")
+            nome = await carro.locator('h2.styles_listingCardContentTitle__AWk2f').inner_text()
+            precos = await carro.locator('span.typo--heading:has-text("R$")').inner_text()
+            print(f"nome: {nome} Preco Atual: {precos}")
         await browser.close()
-asyncio.run(coletando_precos_wemotors())
+
+ 
+
+async def coletando_precos_localiza_seminovos():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page()
+        await page.goto("https://seminovos.localiza.com/carros/pr-curitiba",
+            wait_until="load", timeout=60000)
+        page.set_default_timeout(timeout=60000)
+        card_locator = page.locator('.mui-o9w1ny') # ele ainda nao procurou nada, por isso nao tem await nem inner_text
+        todos_carros = await card_locator.all() # apartir daqui ele varre o hmtl e transforma em uma lista
+        print(f"foram encotrados {len(todos_carros)} cards de carros")
+        for carro in todos_carros:
+            nome = await carro.locator('span.MuiTypography-root').inner_text()
+            precos = await carro.locator('h3.MuiTypography-root:has-text("R$")').inner_text()
+            print(f"nome: {nome} Preco Atual: {precos}")
+        await browser.close()
 
 
 
@@ -38,17 +56,21 @@ async def coletando_precos_chave_na_mao():
         card_locator = page.locator('.style_container__oZoGd')
         todos_carros = await card_locator.all() # all junta tudo carregado em uma lista
         print(f"foram encotrados {len(todos_carros)} cards de carros") # como defini card_locator como lista ele precisa do len, pq nao consegue comparar um numero com uma lista
-        for carro in todos_carros[:50]:
-            await page.mouse.wheel(0, 4000)
-            await page.wait_for_timeout(2000)
-            await carro.scroll_into_view_if_needed()
-            await page.wait_for_timeout(1000)
-            nome = await carro.locator('b.ellipses').inner_text()
-            precos = await carro.locator('p.style_price__e3ffu:has-text("R$")').inner_text()
-            print(f"Nome do Caroo: {nome} - Preco Atual: {precos}")
+        for carro in todos_carros[:20]:
+            try:
+                await page.mouse.wheel(0, 4000)
+                await page.wait_for_timeout(2000)
+                await carro.scroll_into_view_if_needed()
+                await page.wait_for_timeout(1000)
+                nome = await carro.locator('b.ellipses').inner_text()
+                precos = await carro.locator('p.style_price__e3ffu:has-text("R$")').inner_text()
+                print(f"Nome do Caroo: {nome} - Preco Atual: {precos}")
+            except: continue
         await browser.close()
+
+
+
+
+asyncio.run(coletando_precos_napista())
+asyncio.run(coletando_precos_localiza_seminovos())
 asyncio.run(coletando_precos_chave_na_mao())
-
-
-
-
